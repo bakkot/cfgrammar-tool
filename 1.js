beddbe470dd9c961a1f8b3f6c14cf4f4ed8a8755
Kevin Gibbons <bakkot@gmail.com>
2015-05-08 18:16:24 -0700

    html printer

--- 1.js
+++ 1.js
@@ -396,7 +396,7 @@ Grammar.prototype.annotateSelfDeriving = function() {
 }
 
 
-function parse(str, grammar) {
+function parse(str, grammar) { // TODO change order, jeebus
   var queue = [];
   for(var i=0; i<=str.length; ++i) queue.push([]);
   
@@ -589,9 +589,138 @@ function parse(str, grammar) {
   }
   
   
-  return queue;
+  return parses;
 }
 
+
+
+
+// create a DOM node representing the rule. obviously only call in browsers.
+// symbols get class cfg-symbol, the rule itself class cfg-rule.
+function domRule(rule) {
+  var o = document.createElement('span');
+  o.className = 'cfg-rule';
+  
+  var sp = document.createElement('span');
+  sp.className = 'cfg-symbol';
+  sp.appendChild(document.createTextNode(rule.name));
+  o.appendChild(sp);
+  o.appendChild(document.createTextNode(' \u2192 ')); // right arrow
+  
+  if(rule.production.length == 0) {
+    o.appendChild(document.createTextNode('\u025B')); // epsilon
+  }
+  else {
+    for(var i=0; i<rule.production.length; ++i) {
+      if(rule.production[i].type == 'T') {
+        o.appendChild(document.createTextNode(rule.production[i].data));
+      }
+      else {
+        sp = document.createElement('span');
+        sp.className = 'cfg-symbol';
+        sp.appendChild(document.createTextNode(rule.production[i].data));
+        o.appendChild(sp);
+      }
+    }
+  }
+  
+  return o;
+}
+
+// create a DOM table representing the entire parse. obviously only call in browsers.
+function domPrinter(parse) {
+  var str = [parse];
+  
+  function formatIntermediateString(highlightIndex) { // highlightIndex must be a state, not a final symbol
+    var o = document.createElement('span');
+    for(var i=0; i<str.length; ++i) {
+      if(typeof str[i] === 'string') {
+        o.appendChild(document.createTextNode(str[i]));
+      }
+      else {
+        var sp = document.createElement('span');
+        sp.className = 'cfg-symbol';
+        if(i == highlightIndex) {
+          sp.className += ' cfg-rewrite';
+        }
+        sp.appendChild(document.createTextNode(str[i].rule.name));
+        o.appendChild(sp);
+      }
+    }
+    return o;
+  }
+
+
+  var out = document.createElement('table');
+  out.className = 'cfg-derivations derivations'; // TODO second is for compat
+  out.innerHTML = '<thead><tr><th>Rule</th><th>Application</th><th>Result</th></tr></thead>';
+  
+  
+  
+  
+  // handle GAMMA state specially
+  var row = document.createElement('tr');
+  var cell = document.createElement('td');
+  var sp = document.createElement('sp');
+  sp.className = 'cfg-start';
+  sp.appendChild(document.createTextNode('Start'));
+  cell.appendChild(sp);
+  row.appendChild(cell);
+
+  cell = document.createElement('td');
+  var sp = document.createElement('span');
+  sp.className = 'cfg-rule';
+  sp.innerHTML = 'Start \u2192 ' + parse.backPointers[0].rule.name;
+  cell.appendChild(sp);
+  row.appendChild(cell);
+  
+  str = [parse.backPointers[0]]; // ie, start symbol
+  cell = document.createElement('td');
+  cell.appendChild(formatIntermediateString(-1));
+  row.appendChild(cell);
+  
+  out.appendChild(row);
+
+  
+  for(var i = 0; i<str.length; ++i) { // NB: both str.length and i change within the body of the loop
+    if(typeof str[i] === 'string') {
+      continue;
+    }
+    
+    var state = str[i];
+
+    var row = document.createElement('tr');
+    var cell = document.createElement('td');
+    cell.appendChild(domRule(state.rule));
+    row.appendChild(cell);
+  
+    cell = document.createElement('td');
+    cell.appendChild(formatIntermediateString(i));
+    row.appendChild(cell);
+    
+
+    
+    var rewritten = [];
+    for(var j=0; j<state.index; ++j) {
+      if(state.rule.production[j].type == 'T') {
+        rewritten.push(state.rule.production[j].data);
+      }
+      else {
+        rewritten.push(state.backPointers[j]);
+      }
+    }
+    str = str.slice(0, i).concat(rewritten).concat(str.slice(i+1));
+
+    cell = document.createElement('td');
+    cell.appendChild(formatIntermediateString(-1));
+    row.appendChild(cell);
+    out.appendChild(row);
+
+    --i; // gotta reprocess the index we just rewrote
+  }
+  
+  return out;
+}
 
 
 
@@ -629,7 +758,7 @@ var grammar = Grammar([
 
 //console.log(grammar.annotateNullables())
 //console.log(grammar.symbolMap);
-//parse('aabaaabaaa', grammar).join('\n')
+parse('aabaaabaaa', grammar).join('\n')
 
 var grammar = [
   Rule('S', [NT('S'), NT('S')]),
